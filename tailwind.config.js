e25f4c6cee55acd3bd71a05c92e9f27c4bdd20b8
Farvar1511 <RealRodFarva@gmail.com>
2025-04-12 11:29:14 -0400

    style: refactor Tailwind CSS configuration to use ES module syntax

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,12 @@
 // tailwind.config.js
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ["Inter", "sans-serif"], // ⬅️ enables `font-sans`
-        orbitron: ["Orbitron", "sans-serif"], // ⬅️ enables `font-orbitron`
-      },
+export const content = ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"];
+export const theme = {
+  extend: {
+    fontFamily: {
+      sans: ["Inter", "sans-serif"], // ⬅️ enables `font-sans`
+      orbitron: ["Orbitron", "sans-serif"], // ⬅️ enables `font-orbitron`
     },
   },
-  plugins: [],
 };
+export const plugins = [];
